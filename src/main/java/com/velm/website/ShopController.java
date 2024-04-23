42b5e0805fcd6481aa9d5ed18473f8f44778bea5
Gamenwatch412 <81721386+gamenwatch412@users.noreply.github.com>
2024-04-23 15:37:28 -0700

    idk what happended here

--- src/main/java/com/velm/website/ShopController.java
+++ src/main/java/com/velm/website/ShopController.java
@@ -1,10 +1,8 @@
 /**
- * LoginController: This controller class takes any input from any 
- * of the buttons that reference the endpoint of login.html, once it gets that
- * request it takes you to the login.html page
- * @Ethan
- * @ 4/15
- * @ Version 2
+ * This is the ShopController class, which
+ * @ authors: Ethan
+ * @ date of oldest edition: April 17 
+ * @ version: 2 
  */
 
 package com.velm.website;
