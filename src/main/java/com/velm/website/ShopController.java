534e5e563204363c0a7ba37cad24861cffec45c2
Ethan Bolita <ethanbolita2203@gmail.com>
2024-04-23 14:47:51 -0700

    ajaj

--- src/main/java/com/velm/website/ShopController.java
+++ src/main/java/com/velm/website/ShopController.java
@@ -1,3 +1,12 @@
+/**
+ * LoginController: This controller class takes any input from any 
+ * of the buttons that reference the endpoint of login.html, once it gets that
+ * request it takes you to the login.html page
+ * @Ethan
+ * @ 4/15
+ * @ Version 2
+ */
+
 package com.velm.website;
 
 import org.springframework.stereotype.Controller;
