52253f924d8d2f3074ab7a63307ee28874fcfec0
Ethan Bolita <ethanbolita2203@gmail.com>
2024-04-23 15:15:14 -0700

    eeueu

--- src/main/java/com/velm/website/ShopController.java
+++ src/main/java/com/velm/website/ShopController.java
@@ -1,5 +1,4 @@
 /**
-<<<<<<< HEAD
  * LoginController: This controller class takes any input from any 
  * of the buttons that reference the endpoint of login.html, once it gets that
  * request it takes you to the login.html page
@@ -8,13 +7,6 @@
  * @ Version 2
  */
 
-=======
- * This is the ShopController class, which
- * @ authors: Ethan
- * @ date of oldest edition: April 17 
- * @ version: 2 
- */
->>>>>>> origin/main
 package com.velm.website;
 
 import org.springframework.stereotype.Controller;
@@ -24,12 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ShopController {
 
-    // List of products
-    private String[] products = {"Classic Sweatpants", "Sweater", "Women's Shorts"};
-
     @GetMapping("/Shop")
     public String shopPage() {
-        return "Shop"; // Assuming "shop.html" is in the templates directory
+        return "Shop"; 
     }
 
 }
